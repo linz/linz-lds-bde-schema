33cb9bf1b4e5289fb7afa7d58ee9fe07fc89ddba
billgeo <bmnelson@linz.govt.nz>
2016-08-29 17:40:58 +1200

    create survey tables and columns for pending parcels release

--- sql/99-patches.sql
+++ sql/99-patches.sql
@@ -19,6 +19,7 @@ SET client_min_messages TO WARNING;
 SET search_path = lds, bde, public;
 
 DO $PATCHES$
+DECLARE v_patch TEXT;
 BEGIN
 
 IF NOT EXISTS (
@@ -34,6 +35,58 @@ END IF;
 
 -- Patches start from here
 
+-------------------------------------------------------------------------------
+-- 1.0.2 Pending parcels release 
+-------------------------------------------------------------------------------
+
+v_patch := 'BDE - 1.0.2: Create survey tables and columns for pending parcels release';
+
+IF NOT EXISTS (
+    SELECT *
+    FROM   _patches.applied_patches
+    WHERE  patch_name = v_patch
+) THEN
+    SELECT _patches.apply_patch(
+        v_patch,
+        '
+    DO $$
+    BEGIN
+    SET search_path = lds, bde, public;
+    --------------------------------------------------------------------------------
+    -- LDS table affected_parcel_surveys_pend
+    --------------------------------------------------------------------------------
+    DROP TABLE IF EXISTS affected_parcel_surveys_pend CASCADE;
+    CREATE TABLE affected_parcel_surveys_pend (
+        id INTEGER NOT NULL,
+        par_id INTEGER NOT NULL,
+        sur_wrk_id INTEGER NOT NULL,
+        action VARCHAR(12)
+    );
+    ALTER TABLE affected_parcel_surveys_pend ADD PRIMARY KEY (id);
+    ALTER TABLE affected_parcel_surveys_pend OWNER TO bde_dba;
+    REVOKE ALL ON TABLE affected_parcel_surveys_pend FROM PUBLIC;
+    GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE affected_parcel_surveys_pend TO bde_admin;
+    GRANT SELECT ON TABLE affected_parcel_surveys_pend TO bde_user;
+    -- only enable versioning if we already have versioned tables
+    IF EXISTS (SELECT table_version.ver_get_versioned_tables()) THEN
+        PERFORM table_version.ver_enable_versioning(''lds'', ''affected_parcel_surveys_pend'');
+    END IF;
+    --------------------------------------------------------------------------------
+    -- Add lodged_date column to LDS table survey_plans
+    --------------------------------------------------------------------------------
+    IF table_version.ver_is_table_versioned(''lds'', ''survey_plans'') THEN
+        PERFORM table_version.ver_versioned_table_add_column(''lds'', ''survey_plans'', ''lodged_date'', ''timestamp without time zone'');
+        PERFORM table_version.ver_versioned_table_add_column(''lds'', ''survey_plans'', ''authorised_date'', ''timestamp without time zone'');
+    ELSE
+        ALTER TABLE lds.survey_plans ADD COLUMN lodged_date timestamp without time zone;
+        ALTER TABLE lds.survey_plans ADD COLUMN authorised_date timestamp without time zone;
+    END IF;
+    END;
+    $$
+    '
+    );
+END IF;
+
+
 END;
 $PATCHES$;
-
